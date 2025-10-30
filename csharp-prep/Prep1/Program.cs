c2b624abed1a6b0aaa1faf1a92c1baee3bff13df
Viola Hayes <hay22021@byui.edu>
2025-10-30 09:16:50 -0600

    prep 1 c#

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Please enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
-}
+}    
+    
