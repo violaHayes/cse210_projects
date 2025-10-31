76461e4d5d1a1ab2d14de6617dc38efdfd4e2203
Viola Hayes <hay22021@byui.edu>
2025-10-30 19:23:06 -0600

    grade decider thing

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade: ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        string letter = "";
+
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You got a {letter}!");
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the class!");
+        }
+        else
+        {
+                Console.WriteLine("You didn't pass better luck next time!"); 
+        }
+
+    
     }
+
 }
