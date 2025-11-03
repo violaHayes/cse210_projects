24c5abfa9c0f877eaff0ef0d7fdc4fe37c8b9124
Viola Hayes <hay22021@byui.edu>
2025-11-03 09:57:21 -0700

    class work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
     }
 }
+
+    // {
+    //     Console.WriteLine(Math.Pow(2, 64));
+    //     int x = 10;
+    //     Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+
+    //     for (int i = -10000; i <= 10000; i += 100)
+    //     {
+    //         Console.WriteLine($"The value of i is {i}");
+    //     }
+    //     bool done = false;
+    //     while (!done)
+    //     {
+    //         Console.Write("Inout your age: ");
+    //         int age = int.Parse(Console.ReadLine());
+    //         if (age >= 0 && age <= 125)
+    //         {
+    //             done = true;
+    //             Console.Write($"Super age: {age}");
+    //         }
+    //     }
+    //     done = false;
+    //     do
+    //     {
+
+    //         Console.Write("Inout your age: ");
+    //         int age = int.Parse(Console.ReadLine());
+    //         if (age >= 0 && age <= 125)
+    //         {
+    //             done = true;
+    //             Console.Write($"Super age: {age}");
+    //             while (!done);
+
+
+
+
+            
+        
+
+    
+  
