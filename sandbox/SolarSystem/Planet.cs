a802866526d1c9899db96b3aa2e0d9a9596e624e
Viola Hayes <hay22021@byui.edu>
2025-11-04 10:02:32 -0700

    solar planets

---
+++ sandbox/SolarSystem/Planet.cs
@@ -0,0 +1,10 @@
+class Planet
+{
+    public string _name="";
+    public double _diameter;
+
+    public void DisplayPlanetInformation()
+    {
+       Console.WriteLine($"The planet name: {_name}, diameter: {_diameter}"); 
+    }
+}
--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,17 @@
 {
     static void Main()
     {
-        Console.Write("Bonjour le something ");
+        // Console.Write("Bonjour le something ");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 1234.343;
+
+        mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        mercury._name = "Venus";
+        mercury._diameter = 1232344.343;
+
+        mercury.DisplayPlanetInformation();
     }
 }
