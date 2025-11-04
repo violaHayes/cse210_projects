d74d33de65fc0f451ec39fd8d009f3997662a64e
Viola Hayes <hay22021@byui.edu>
2025-11-04 09:45:20 -0700

    intial solar system project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,49 +2,56 @@ using System;
 
 class Program
 {
+    static int AddValues(int n1, int n2, int n3)
+    {
+        return n1 + n2 + n3;
+    }
+    
+    static void TestValues(int x, float y, double z)
+    {
+        Console.WriteLine($"The values are: {x}, {y}, {z}");
+    }
+
     static void Main(string[] args)
     {
+        TestValues(10, (float)123, 234.234); 
+        
+        int total = AddValues(10, 20, 30);
+        Console.WriteLine($"The total is: {total}");
 
+        // {
+        //     Console.WriteLine(Math.Pow(2, 64));
+        //     int x = 10;
+        //     Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+
+        //     for (int i = -10000; i <= 10000; i += 100)
+        //     {
+        //         Console.WriteLine($"The value of i is {i}");
+        //     }
+        //     bool done = false;
+        //     while (!done)
+        //     {
+        //         Console.Write("Inout your age: ");
+        //         int age = int.Parse(Console.ReadLine());
+        //         if (age >= 0 && age <= 125)
+        //         {
+        //             done = true;
+        //             Console.Write($"Super age: {age}");
+        //         }
+        //     }
+        //     done = false;
+        //     do
+        //     {
+
+        //         Console.Write("Inout your age: ");
+        //         int age = int.Parse(Console.ReadLine());
+        //         if (age >= 0 && age <= 125)
+        //         {
+        //             done = true;
+        //             Console.Write($"Super age: {age}");
+        //             while (!done);
+        //         }
+        //     } while (!done); // Corrected the do-while syntax here
+        // }
     }
 }
-
-    // {
-    //     Console.WriteLine(Math.Pow(2, 64));
-    //     int x = 10;
-    //     Console.WriteLine($"{x}, {++x}, {x++}, {x}");
-
-    //     for (int i = -10000; i <= 10000; i += 100)
-    //     {
-    //         Console.WriteLine($"The value of i is {i}");
-    //     }
-    //     bool done = false;
-    //     while (!done)
-    //     {
-    //         Console.Write("Inout your age: ");
-    //         int age = int.Parse(Console.ReadLine());
-    //         if (age >= 0 && age <= 125)
-    //         {
-    //             done = true;
-    //             Console.Write($"Super age: {age}");
-    //         }
-    //     }
-    //     done = false;
-    //     do
-    //     {
-
-    //         Console.Write("Inout your age: ");
-    //         int age = int.Parse(Console.ReadLine());
-    //         if (age >= 0 && age <= 125)
-    //         {
-    //             done = true;
-    //             Console.Write($"Super age: {age}");
-    //             while (!done);
-
-
-
-
-            
-        
-
-    
-  
---
+++ sandbox/SolarSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Bonjour le something ");
+    }
+}
