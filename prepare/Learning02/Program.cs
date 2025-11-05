6c0ff88e5e6d1d1579080026a98391f7999aa119
Viola Hayes <hay22021@byui.edu>
2025-11-05 15:34:36 -0700

    finished abstractions learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+        Job job1 = new Job();
+        job1._jobTitle = "Electrical Engineer";
+        job1._company = "INL";
+        job1._startYear = 2013;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Teacher";
+        job2._company = "BYU-I";
+        job2._startYear = 2016;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._userName = "Viola Hayes";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }   
 }
