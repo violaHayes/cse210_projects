2da16cb343c47812a4c64faa3c8ebaae236d6645
Viola Hayes <hay22021@byui.edu>
2025-11-04 20:55:45 -0700

    magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,57 +1,88 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
-    static int AddValues(int n1, int n2, int n3)
-    {
-        return n1 + n2 + n3;
-    }
-    
-    static void TestValues(int x, float y, double z)
-    {
-        Console.WriteLine($"The values are: {x}, {y}, {z}");
-    }
-
     static void Main(string[] args)
     {
-        TestValues(10, (float)123, 234.234); 
-        
-        int total = AddValues(10, 20, 30);
-        Console.WriteLine($"The total is: {total}");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
-        // {
-        //     Console.WriteLine(Math.Pow(2, 64));
-        //     int x = 10;
-        //     Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+        int guess = -1;
 
-        //     for (int i = -10000; i <= 10000; i += 100)
-        //     {
-        //         Console.WriteLine($"The value of i is {i}");
-        //     }
-        //     bool done = false;
-        //     while (!done)
-        //     {
-        //         Console.Write("Inout your age: ");
-        //         int age = int.Parse(Console.ReadLine());
-        //         if (age >= 0 && age <= 125)
-        //         {
-        //             done = true;
-        //             Console.Write($"Super age: {age}");
-        //         }
-        //     }
-        //     done = false;
-        //     do
-        //     {
 
-        //         Console.Write("Inout your age: ");
-        //         int age = int.Parse(Console.ReadLine());
-        //         if (age >= 0 && age <= 125)
-        //         {
-        //             done = true;
-        //             Console.Write($"Super age: {age}");
-        //             while (!done);
-        //         }
-        //     } while (!done); // Corrected the do-while syntax here
-        // }
+        while (guess != magicNumber)
+        {
+            Console.Write("What is the magic number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
+
+
     }
+    // static int AddValues(int n1, int n2, int n3)
+    // {
+    //     return n1 + n2 + n3;
+    // }
+
+    // static void TestValues(int x, float y, double z)
+    // {
+    //     Console.WriteLine($"The values are: {x}, {y}, {z}");
+    // }
+
+    // static void Main(string[] args)
+    // {
+    //     TestValues(10, (float)123, 234.234); 
+
+    //     int total = AddValues(10, 20, 30);
+    //     Console.WriteLine($"The total is: {total}");
+
+    // {
+    //     Console.WriteLine(Math.Pow(2, 64));
+    //     int x = 10;
+    //     Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+
+    //     for (int i = -10000; i <= 10000; i += 100)
+    //     {
+    //         Console.WriteLine($"The value of i is {i}");
+    //     }
+    //     bool done = false;
+    //     while (!done)
+    //     {
+    //         Console.Write("Inout your age: ");
+    //         int age = int.Parse(Console.ReadLine());
+    //         if (age >= 0 && age <= 125)
+    //         {
+    //             done = true;
+    //             Console.Write($"Super age: {age}");
+    //         }
+    //     }
+    //     done = false;
+    //     do
+    //     {
+
+    //         Console.Write("Inout your age: ");
+    //         int age = int.Parse(Console.ReadLine());
+    //         if (age >= 0 && age <= 125)
+    //         {
+    //             done = true;
+    //             Console.Write($"Super age: {age}");
+    //             while (!done);
+    //         }
+    //     } while (!done); // Corrected the do-while syntax here
+    // }
 }
