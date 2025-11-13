aa33aafad4ac4860f7d880d3d2cb839e52decbe4
Viola Hayes <hay22021@byui.edu>
2025-11-13 09:20:34 -0700

    display word

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Word myWord = new Word("Uncle");
+        myWord.DisplayWord();
     }
 }
--- prove/Develop03/word.cs
+++ prove/Develop03/word.cs
@@ -1,1 +1,19 @@
-class Word;
+using System.Runtime.CompilerServices;
+
+class Word
+{
+    private string _word;
+
+private bool _isHidden;
+
+    public Word(string word)
+    {
+        _word = word;
+        _isHidden = false;
+    }
+    public void DisplayWord()
+    {
+        Console.WriteLine(_word);
+    }
+
+}
