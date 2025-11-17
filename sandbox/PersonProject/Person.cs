9c53b508339ff217d8b0b0b5ea831ab8670e1ec1
Viola Hayes <hay22021@byui.edu>
2025-11-17 10:31:03 -0700

    presonproject

---
+++ sandbox/PersonProject/Person.cs
@@ -0,0 +1,34 @@
+// class Person
+// {
+//     static void Main(string[] args)
+//     {}
+        
+    
+//     private string _firstName;
+//     private string _lastName;
+//     private int _age;
+//     private int _weight;
+
+//     public void Person(string firstName, string lastName, int age, int weight)
+//     {
+//         _firstName = firstName;
+//         _lastName = lastName;
+//         _age = age;
+//         _weight = weight;
+//     }
+ 
+//     public void SetAge(int age)
+//     {
+//         if (age <=0)
+//         return;
+//         _age = age;
+
+//     }
+//     public string PersonalInfo()
+//     {
+//         return $"{_lastName}, {_firstName}, is {_age} years old and weighs {_weight}lbs";
+//     }
+
+
+//     }
+
---
+++ sandbox/PersonProject/Police.cs
@@ -0,0 +1,15 @@
+// class Police: Person
+// {
+//     private string _weapons;
+//     public Police(string weapons, string firstName, string lastName, int age, int weight)
+//     : base (firstName, lastName, age, weight)
+//     {
+//         _weapons = weapons;
+//     }
+//     public string GetPoliceInformation()
+//     {
+//         return $"{base.GetPersonInformation()}, {_weapons}";
+//     }
+
+    
+// }
