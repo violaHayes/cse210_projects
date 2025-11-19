5ffc78fdd75018220ffe4003769e0a8b93397bdf
Viola Hayes <hay22021@byui.edu>
2025-11-18 21:30:14 -0700

    inital mindfullness activity code

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("-");
+
+        Thread.Sleep(500);
+
+        Console.Write("\b \b"); // Erase the + character
+        Console.Write("?"); // Replace it with the - character
+        Console.Write("\b \b"); // Erase the + character
+        Console.Write("^"); 
+        Console.Write("\b \b"); // Erase the + character
+        Console.Write("*"); 
+
         Console.WriteLine("Hello Develop04 World!");
     }
 }
