6f4ce4c2ac4f0bfe9c1e35822797ac9b083af5ff
Viola Hayes <hay22021@byui.edu>
2025-11-25 22:56:46 -0700

    learning 5 activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
+{
+    List<Shape> shapes = new List<Shape>();
+
+    shapes.Add(new Square("Blue", 5));
+    shapes.Add(new Rectangle("Red", 4, 6));
+    shapes.Add(new Circle("Green", 3));
+
+    foreach (Shape s in shapes)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Color: " + s.GetColor());
+        Console.WriteLine("Area: " + s.GetArea());
+        Console.WriteLine();
     }
+}
 }
