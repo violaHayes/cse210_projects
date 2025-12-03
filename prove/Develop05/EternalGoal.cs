4d0e0dcb13ad2bb5d470f18e84413d68200f823d
Viola Hayes <hay22021@byui.edu>
2025-12-03 10:15:08 -0700

    eternal

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,5 +1,14 @@
-public class Eternal
+public class Eternal: Goal
 {
-    
+    public Eternal(string name, string description, int points) : base(name, description, points)
+    {
+        
+    }
+    public Eternal(string name, string description, int points, int timesRecorded) : base(name, description, points)
+    {
+        
+    }
+    public override
+
     
 }
