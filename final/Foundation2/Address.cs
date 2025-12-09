c13d164e520df558f91211828d68b1bae9431181
Viola Hayes <hay22021@byui.edu>
2025-12-09 09:45:49 -0700

    address class

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -2,11 +2,21 @@ using System.Data.SqlTypes;
 
 class Address
 {
+    private string _name;
      private string _street;
      private string _city;
      private string _state;
      private string _zipCode;
      private string _country;
      private bool _inUSA;
-     
+
+     public void GetFullAddress()
+    {
+        Console.WriteLine($"Address: {_street}");
+        Console.WriteLine($"{_city}, {_state}, {_zipCode}");
+        Console.WriteLine($"{_country}");
+
+    }
+
+
 }
